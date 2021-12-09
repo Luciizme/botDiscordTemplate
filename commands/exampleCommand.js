f6d6b41a647bad389cdff6148f9178c1f6daae34
Lucifer <79420176+Luciizme@users.noreply.github.com>
2021-12-10 05:56:30 +0700

    Create exampleCommand.js

---
+++ commands/exampleCommand.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require('discord.js')
+exports.run = async (client, message) => {
+ 
+};
+
+exports.conf = {
+  aliases: ["exam"]
+};
+exports.help = {
+  name: "example",
+  description: "example",
+  usage: "example"
+};
